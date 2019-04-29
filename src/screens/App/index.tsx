3d4d0373b0e560774b172313325ec272e8277e25
Ethan Neff <ethan.neff@eneff.com>
2019-04-29 13:45:59 -0700

    fix: update depedencies

--- src/screens/App/index.tsx
+++ src/screens/App/index.tsx
@@ -14,9 +14,8 @@ const styles = StyleSheet.create({
 export const App = () => {
   const instructions = Platform.select({
     android:
-      "Double tap R on your keyboard to reload,\n" +
-      "Shake or press menu button for dev menu",
-    ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu"
+      "Double tap R on your keyboard to reload,\nShake or press menu button for dev menu",
+    ios: "Press Cmd+R to reload,\nCmd+D or shake for dev menu"
   });
 
   return (
