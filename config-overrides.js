d0903f87347e5cb6a3a2db247ee081e0603aed4a
Pablo Espinosa <pablo.bermejo@immediate.co.uk>
2019-11-09 20:53:25 +0000

    Improved config overrides

--- config-overrides.js
+++ config-overrides.js
@@ -18,12 +18,8 @@ module.exports = override(
     "react-native-svg": "svgs" // not necessary unless you wanted to do this
   }),
   babelInclude([
-    path.resolve("src") // make sure you link your own source
-    // any react-native modules you need babel to compile
-    // path.resolve("node_modules/react-native-indicators"),
-    // path.resolve("node_modules/react-native-popup-menu"),
-    // path.resolve("node_modules/react-native-vector-icons"),
-    // path.resolve("node_modules/react-native-web-linear-gradient"),
-    // path.resolve("node_modules/react-router-native")
+    path.resolve("src"), // make sure you link your own source
+    // any react-native modules you need babel to compile i.e
+    // path.resolve("node_modules/react-native-vector-icons")
   ])
 );
