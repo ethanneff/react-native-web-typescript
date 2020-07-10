a4a18257f5508d4e6c5582f019f6be399576e28b
Ethan Neff <ethan.neff@eneff.com>
2020-07-10 06:16:59 -0700

    fix: add global alert and notification

---
+++ src/providers/Navigation/configs.ts
@@ -0,0 +1,24 @@
+import {StackNavigationOptions} from '@react-navigation/stack';
+import {Platform} from 'react-native';
+
+export const rootMode = Platform.OS === 'web' ? 'card' : 'modal';
+export const rootScreenOptions: StackNavigationOptions = {
+  headerShown: false,
+  cardStyle: {backgroundColor: 'transparent'},
+  cardOverlayEnabled: true,
+  cardStyleInterpolator: ({current: {progress}}) => ({
+    cardStyle: {
+      opacity: progress.interpolate({
+        inputRange: [0, 0.5, 0.9, 1],
+        outputRange: [0, 0.25, 0.7, 1],
+      }),
+    },
+    overlayStyle: {
+      opacity: progress.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 0.5],
+        extrapolate: 'clamp',
+      }),
+    },
+  }),
+};
--- src/providers/Navigation/index.tsx
+++ src/providers/Navigation/index.tsx
@@ -8,3 +8,10 @@ const SignInStack = () => (
 );
   const token = useRootSelector((state) => state.auth.token);
   const appStack = token ? MainStack : SignInStack;
+      <Stack.Navigator mode={rootMode} screenOptions={rootScreenOptions}>
+        <Stack.Screen
+          name="GlobalNotification"
+          component={GlobalNotification}
+        />
+        <Stack.Screen name="GlobalAlert" component={GlobalAlert} />
+      </Stack.Navigator>
