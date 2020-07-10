3cdacc006f1630d71a42a1b906ee7eb9dc1d53e9
Ethan Neff <ethan.neff@eneff.com>
2020-07-10 06:12:28 -0700

    style: eslint single line

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   root: true,
   extends: ['@react-native-community'],
-  env: {
-    jest: true,
-  },
+  env: {jest: true},
   ignorePatterns: ['node_modules/', 'coverage/', 'android/', 'ios/'],
 };
